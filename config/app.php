19ffd799ab26e6acc6013f964769563211743060
JayMarPabayo <jaymarpabayo@gmail.com>
2025-08-09 16:45:46 +0800

    login and pictures updates

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Michael Ho Events Styling And Coordination'),
 
     /*
     |--------------------------------------------------------------------------
