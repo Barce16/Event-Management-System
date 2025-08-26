4ee89b643858e688f2369a42618efd4486839d9f
JayMarPabayo <jaymarpabayo@gmail.com>
2025-08-26 13:40:45 +0800

    web logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700|playfair-display:600" rel="stylesheet" />
-
+    <link rel="icon" type="image/png" href="{{ asset('images/favicon.png') }}">
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @endif
